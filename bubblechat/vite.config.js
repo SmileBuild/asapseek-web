a1a96033e8010e9926bbca0977e0e0b6cf5f3fb6
haoyuner66 <2232468755@qq.com>
2025-03-16 22:28:18 +0800

    添加导出功能

--- bubblechat/vite.config.js
+++ bubblechat/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+      ]
+    }
+  },
   server: {
     port: 5173,
     open: true,
